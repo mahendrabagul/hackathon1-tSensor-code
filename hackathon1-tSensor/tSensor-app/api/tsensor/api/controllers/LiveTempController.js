613f185bf778c0e9af6aca6d3191380ae4d15043
encode101 <rahul_mishra@neovasolutions.com>
2014-12-20 16:06:55 +0530

    First Phase Of tSensor Done

---
+++ hackathon1-tSensor/tSensor-app/api/tsensor/api/controllers/LiveTempController.js
@@ -0,0 +1,12 @@
+/**
+ * LiveTempController
+ *
+ * @description :: Server-side logic for managing Livetemps
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+
+};
+
